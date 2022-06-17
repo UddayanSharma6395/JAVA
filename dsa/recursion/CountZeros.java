133f74cd407d3722d157927a7013143f4d64fd18
Uddayan Sharma <63939484+UddayanSharma6395@users.noreply.github.com>
2022-06-17 13:27:12 +0530

    Update and rename CountZeroes.java to CountZeros.java

--- dsa/recursion/CountZeros.java
+++ dsa/recursion/CountZeros.java
@@ -3,7 +3,7 @@
 WAP Count Number of Zero Digit in a given number e.g 1209803 Result 2
 */
 
-public class CountZeroes {
+public class CountZeros {
     public static void main(String[] args) {
         int n=1902305;
         System.out.println("the number of zeroe's are : "+countZeros(n));
